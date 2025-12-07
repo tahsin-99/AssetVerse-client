b3a59f0b1d3fefb12e930a8fe06e9cab097ac37d
tahsin-99 <somethinglearn53@gmail.com>
2025-12-07 23:24:14 +0600

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import Container from './Container';
+import { Link } from 'react-router';
+import { AiOutlineMenu } from 'react-icons/ai';
+import useAuth from '../hooks/useAuth';
+import  unknown from '../assets/images/unknownUser.png'
+
+const Navbar = () => {
+      const { user, logOut } = useAuth()
+  const [isOpen, setIsOpen] = useState(false)
+    return (
+        <div>
+            <div className='fixed w-full bg-white z-10 shadow-sm'>
+      <div className='py-4 '>
+        <Container>
+          <div className='flex flex-row  items-center justify-between gap-3 md:gap-0'>
+            {/* Logo */}
+            <Link to='/'>
+              <p className='text-5xl font-bold'>AssetVerse</p>
+            </Link>
+            {/* Dropdown Menu */}
+            <div className='relative'>
+              <div className='flex flex-row items-center gap-3'>
+                {/* Dropdown btn */}
+                <div
+                  onClick={() => setIsOpen(!isOpen)}
+                  className='p-4 md:py-1 md:px-2 border border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'
+                >
+                  <AiOutlineMenu />
+                  <div className='hidden md:block'>
+                    {/* Avatar */}
+                    <img
+                      className='rounded-full'
+                      referrerPolicy='no-referrer'
+                      src={user && user.photoURL ? user.photoURL : unknown}
+                      alt='profile'
+                      height='30'
+                      width='30'
+                    />
+                  </div>
+                </div>
+              </div>
+              {isOpen && (
+                <div className='absolute rounded-xl shadow-md w-[40vw] md:w-[10vw] bg-white overflow-hidden right-0 top-12 text-sm'>
+                  <div className='flex flex-col cursor-pointer'>
+                    <Link
+                      to='/'
+                      className='block md:hidden px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                    >
+                      Home
+                    </Link>
+
+                    {user ? (
+                      <>
+                        <Link
+                          to='/dashboard'
+                          className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                        >
+                          Dashboard
+                        </Link>
+                        <div
+                          onClick={logOut}
+                          className='px-4 py-3 hover:bg-neutral-100 transition font-semibold cursor-pointer'
+                        >
+                          Logout
+                        </div>
+                      </>
+                    ) : (
+                      <>
+                        <Link
+                          to='/login'
+                          className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                        >
+                          Login
+                        </Link>
+                        <Link
+                          to='/register'
+                          className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                        >
+                          Sign Up
+                        </Link>
+                      </>
+                    )}
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/Routes/router.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router";
+import HomeLayout from "../Layout/HomeLayout/HomeLayout";
+import Home from "../Components/Home";
+import Login from "../Auth/login";
+import Register from "../Auth/register";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:HomeLayout,
+    children:[
+       {
+         path:'/',
+        Component:Home
+    }
+    ]
+  },
+  
+  {
+    path:'/register',
+    Component:Register
+  },
+  {
+    path:'/login',
+    Component:Login
+  },
+
+]);
