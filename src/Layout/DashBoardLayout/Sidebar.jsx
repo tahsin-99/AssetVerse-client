55ee39e497a6c4988ff6604498600b061de2682f
tahsin-99 <somethinglearn53@gmail.com>
2025-12-08 21:53:12 +0600

    dashboard-done

--- src/Layout/DashBoardLayout/Sidebar.jsx
+++ src/Layout/DashBoardLayout/Sidebar.jsx
@@ -114,8 +114,8 @@ const Sidebar = () => {
             <li>
               <NavLink
                 className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-                data-tip="Payment History"
-                to="/dashboard/payment-history"
+                data-tip="Payment"
+                to="/dashboard/payment"
               >
                 <MdOutlinePayment />
 
@@ -128,7 +128,7 @@ const Sidebar = () => {
                 <NavLink
                   className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
                   data-tip="Affiliated Employees"
-                  to="/dashboard/affiliated-employee"
+                  to="/dashboard/affiliated-employees"
                 >
                   <FaTasks></FaTasks>
 
@@ -158,7 +158,7 @@ const Sidebar = () => {
                 <NavLink
                   className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
                   data-tip="Manage Employees"
-                  to="/dashboard/manage-employee"
+                  to="/dashboard/manage-employees"
                 >
                   <GrUserManager />
 
@@ -195,31 +195,7 @@ const Sidebar = () => {
               </div>
             </li>
 
-            {/* List item */}
-            <li>
-              <button
-                className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
-                data-tip="Settings"
-              >
-                {/* Settings icon */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2"
-                  fill="none"
-                  stroke="currentColor"
-                  className="my-1.5 inline-block size-4"
-                >
-                  <path d="M20 7h-9"></path>
-                  <path d="M14 17H5"></path>
-                  <circle cx="17" cy="17" r="3"></circle>
-                  <circle cx="7" cy="7" r="3"></circle>
-                </svg>
-                <span className="is-drawer-close:hidden">Settings</span>
-              </button>
-            </li>
+           
           </ul>
         </div>
       </div>
--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -4,6 +4,12 @@ import Home from "../Components/Home";
 import Login from "../Auth/login";
 import Register from "../Auth/register";
 import Dashboard from "../Layout/DashBoardLayout/Dashboard";
+import AddAssets from "../Layout/DashBoardLayout/MenuPages/AddAssets";
+import AffiliatedEmployees from "../Layout/DashBoardLayout/MenuPages/AffiliatedEmployees";
+import ManageEmployees from "../Layout/DashBoardLayout/MenuPages/ManageEmployees";
+import RequestAssets from "../Layout/DashBoardLayout/MenuPages/RequestAssets";
+import Payment from "../Layout/DashBoardLayout/MenuPages/Payment";
+import Profile from "../Layout/DashBoardLayout/MenuPages/Profile";
 
 
 export const router = createBrowserRouter([
@@ -30,7 +36,34 @@ export const router = createBrowserRouter([
   {
     path:'/dashboard',
     Component:Dashboard,
-    
+    children:[
+      {
+        path:'add-assets',
+        Component:AddAssets
+      },
+      {
+         
+        path:'affiliated-employees',
+        Component:AffiliatedEmployees
+      
+      },
+      {
+         path:'manage-employees',
+        Component:ManageEmployees
+      },
+      {
+         path:'request-assets',
+        Component:RequestAssets
+      },
+      {
+         path:'payment',
+        Component:Payment
+      },
+      {
+         path:'profile',
+        Component:Profile
+      }
+    ]
   }
 
 ]);
